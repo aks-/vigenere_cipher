7bef5e357e829134b0de9dc1749a0873b48f5fb8
aks- <aks-@users.noreply.github.com>
2014-08-01 16:00:12 +0530

    fix test

--- spec/vigenere_cipher_spec.rb
+++ spec/vigenere_cipher_spec.rb
@@ -1,23 +1,33 @@
 require 'vigenere_cipher'
 
-describe 'vigenere cipher' do
-  it 'should work for simple strings' do
-    vigenere_encrypt('LEMON', 'ATTACK AT DAWN').should == 'LXFOPVEFRNHR'
-    vigenere_decrypt('LEMON', 'LXFOPVEFRNHR').should == 'ATTACKATDAWN'
+describe Vigenere do
+  class TestClass
   end
 
-  it 'should be case-insensitive' do
-    vigenere_encrypt('LEMON', 'ATtack aT DawN').should == 'LXFOPVEFRNHR'
-    vigenere_decrypt('LEMON', 'LXFOPVEFRNHR').should == 'ATTACKATDAWN'
+  before(:all) do
+    @test = TestClass.new
+    @test.extend Vigenere
   end
 
-  it 'should ignore all the puncuations and non characters' do
-    vigenere_encrypt('LEMON', 'ATtack! - aT   \ Da-|wN').should == 'LXFOPVEFRNHR'
-    vigenere_decrypt('LEMON', 'LXFOPVEFR--N|HR').should == 'ATTACKATDAWN'
-  end
+  describe 'vigenere cipher tests' do
+    it 'should work for simple strings' do
+      @test.vigenere_encrypt('LEMON', 'ATTACK AT DAWN').should == 'LXFOPVEFRNHR'
+      @test.vigenere_decrypt('LEMON', 'LXFOPVEFRNHR').should == 'ATTACKATDAWN'
+    end
 
-  it 'should raise an error when either key or text is empty' do
-    expect{ vigenere_encrypt('', 'A') }.to raise_error(NameError)
-    expect{ vigenere_decrypt('B', '') }.to raise_error(NameError)
+    it 'should be case-insensitive' do
+      @test.vigenere_encrypt('LEMON', 'ATtack aT DawN').should == 'LXFOPVEFRNHR'
+      @test.vigenere_decrypt('LEMON', 'LXFOPVEFRNHR').should == 'ATTACKATDAWN'
+    end
+
+    it 'should ignore all the puncuations and non characters' do
+      @test.vigenere_encrypt('LEMON', 'ATtack! - aT   \ Da-|wN').should == 'LXFOPVEFRNHR'
+      @test.vigenere_decrypt('LEMON', 'LXFOPVEFR--N|HR').should == 'ATTACKATDAWN'
+    end
+
+    it 'should raise an error when either key or text is empty' do
+      expect{ @test.vigenere_encrypt('', 'A') }.to raise_error(NameError)
+      expect{ @test.vigenere_decrypt('B', '') }.to raise_error(NameError)
+    end
   end
 end
