85ae6ca821c38916205923dfbb267829826f4473
aks- <aks-@users.noreply.github.com>
2014-08-01 11:47:04 +0530

    complete viginere cipher

--- lib/vigenere_cipher.rb
+++ lib/vigenere_cipher.rb
@@ -4,7 +4,7 @@ SIZE = 26
 START = 'A'.unpack('C').first
 
 def filter data
-  data.upcase.gsub(/[^A-Z]/, '')
+  data.upcase.gsub /[^A-Z]/, ''
 end
 
 def vigenere_encrypt key, text
@@ -15,15 +15,18 @@ def vigenere_decrypt key, text
   vigenere key, text, DECRYPT_FUNC
 end
 
+def iterator key
+  key.chars.map {|character| character.unpack('C').first - START }.cycle
+end
+
+
 def vigenere key, text, func
   raise NameError, 'Please provide valid key and text' if text.empty? or key.empty?
-  key_to_ascii = (filter key).chars.map{|character| character.unpack('C').first - START}
-  plain_text = filter text.upcase
+  iterator = iterator filter key
+  plain_text = filter text
   plain_text.each_char.inject('') do |encrypted_text, encrypted_letter|
-    key_to_ascii = key_to_ascii.cycle
-    position = key_to_ascii.next
-    encrypted_text += ((encrypted_letter.unpack('C').first - START).send(func, position) % SIZE).chr
+    position = iterator.next
+    encrypted_text += ((encrypted_letter.unpack('C').first - START).send(func, position) % SIZE + START).chr
   end
 end
 
-vigenere_encrypt 'eh', 'bok'
